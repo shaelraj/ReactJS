4b94008f20d456576433d8abb393ae56e593bc5e
shaelraj <shael.dhum88@gmail.com>
2021-02-13 19:51:48 +0530

    React component changes

---
+++ React/hello-world/src/Components/MyGreet.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+class MyGreet extends Component {
+
+    render (){
+        return (
+            <div>
+            Hello from Class components!!!
+            </div>
+
+        );
+    }
+}
+
+export default MyGreet;

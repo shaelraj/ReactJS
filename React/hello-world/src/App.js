bc57170bf174c81fe87054f3038092d0350ee937
shaelraj <shael.dhum88@gmail.com>
2021-02-14 21:51:33 +0530

    React state and setState concept

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Greet from "./Components/Greet";
 import MyGreet from "./Components/MyGreet";
 import Hello from "./Components/Hello";
+import Message from "./Components/Message";
+import Counter from "./Components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="shaelraj">
+    { /* <Greet name="shaelraj">
         <p>This is props</p>
       </Greet>
       <Greet name="shaelemor" />
@@ -15,7 +17,10 @@ function App() {
       <MyGreet name="shaelraj" />
       <MyGreet name="shaelemor" />
       <MyGreet name="parizval" />
-      <Hello />
+  <Hello /> */}
+
+     { /*  <Message />*/}
+     <Counter/>
     </div>
   );
 }
